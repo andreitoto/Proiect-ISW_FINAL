74a8b2ff3d18e5ee3526002258e5e4bdbbde00a8
RazvanDob <100287012+RazvanDob@users.noreply.github.com>
2023-05-04 21:13:22 +0300

    Update number_plate.py

--- number_plate.py
+++ number_plate.py
@@ -15,8 +15,8 @@ harcascade = "model\haarcascade_russian_plate_number.xml"
 
 cap = cv2.VideoCapture(0)
 
-cap.set(3, 640) 
-cap.set(4, 480) 
+cap.set(3, 640) #width 
+cap.set(4, 480)  #height 
 
 min_area = 500
 count = 0
